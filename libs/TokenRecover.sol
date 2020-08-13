b8834ebffc4a8d2190a6987442ea4ebaea5bf19f
DefiOfThrones <gameofdefi@gmail.com>
2020-08-13 14:40:10 +0200

    Add DotToken & libs

---
+++ libs/TokenRecover.sol
@@ -0,0 +1,22 @@
+// File: eth-token-recover/contracts/TokenRecover.sol
+
+pragma solidity ^0.6.0;
+
+
+
+/**
+ * @title TokenRecover
+ * @author Vittorio Minacori (https://github.com/vittominacori)
+ * @dev Allow to recover any ERC20 sent into the contract for error
+ */
+contract TokenRecover is Ownable {
+
+    /**
+     * @dev Remember that only owner can call so be careful when use on contracts generated from other contracts.
+     * @param tokenAddress The token contract address
+     * @param tokenAmount Number of tokens to be sent
+     */
+    function recoverERC20(address tokenAddress, uint256 tokenAmount) public onlyOwner {
+        IERC20(tokenAddress).transfer(owner(), tokenAmount);
+    }
+}
