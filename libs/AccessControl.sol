164d18df175e0d2825cb534da5da91cf301b70ae
DefiOfThrones <gameofdefi@gmail.com>
2020-08-13 15:19:46 +0200

    Add Address import

--- libs/AccessControl.sol
+++ libs/AccessControl.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.6.0;
 
 import "./Context.sol";
 import "./EnumerableSet.sol";
+import "./Address.sol";
 
 /**
  * @dev Contract module that allows children to implement role-based access
