0d37ac98cf5a0a66676690339f5bf40e006daa17
Maxime Reynders <maximereynders@gmail.com>
2021-02-09 20:48:22 +0100

    * Working on DoTxNft.sol

--- DoTxNft.sol
+++ DoTxNft.sol
@@ -1,44 +1,59 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
-import "./ERC721PresetMinterPauser.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "https://raw.githubusercontent.com/DefiOfThrones/DOTTokenContract/master/nft/AccessControl.sol";
+import "https://raw.githubusercontent.com/DefiOfThrones/DOTTokenContract/master/nft/ERC721Burnable.sol";
+import "https://raw.githubusercontent.com/DefiOfThrones/DOTTokenContract/master/nft/ERC721Pausable.sol";
 
-contract DoTxNFT is ERC721PresetMinterPauser, Ownable {
-    bytes32 public constant MODEL_CREATOR_ROLE =
-        keccak256("MODEL_CREATOR_ROLE");
+contract DoTxNFT is ERC721, AccessControl, ERC721Burnable, ERC721Pausable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    
+    bytes32 public constant HOUSE_CREATOR_ROLE = keccak256("HOUSE_CREATOR_ROLE");
 
-    mapping(uint256 => uint256) public nextId;
+    mapping(uint256 => uint256) public nextHouseId;
     mapping(uint256 => uint256) public supply;
+    
 
-    uint32 public constant ID_TO_MODEL = 1000000;
-    event NewModel(uint256 id, uint256 maxSupply);
+    uint32 public constant ID_TO_HOUSE = 1000000;
+    event NewHouse(uint256 id, uint256 maxSupply);
 
-    constructor()
-        public
-        ERC721PresetMinterPauser(
-            "DeFi of Thrones NFT",
-            "DoTxNFT",
-            "https://nfttest.defiofthrones.io/"
-        )
-    {}
+    constructor() public ERC721("DeFi of Thrones NFT", "DoTxNFT"){
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
 
+        _setBaseURI("https://nfttest.defiofthrones.io/");
+    }
+    
     function newModel(uint256 id, uint256 maxSupply) external {
-        require(
-            hasRole(MODEL_CREATOR_ROLE, _msgSender()),
-            "DoTxNFT: require model creator role"
-        );
-        require(maxSupply <= ID_TO_MODEL, "DoTxNFT: max supply too high");
-        require(supply[id] == 0, "DoTxNFT: model already exist");
+        require(hasRole(HOUSE_CREATOR_ROLE, _msgSender()), "DoTxNFT: require house creator role");
+        require(maxSupply <= ID_TO_HOUSE, "DoTxNFT: max supply too high");
+        require(supply[id] == 0, "DoTxNFT: house already exist");
 
         supply[id] = maxSupply;
-        NewModel(id, maxSupply);
+        NewHouse(id, maxSupply);
+    }
+    
+    function mint(address to, uint256 houseId) public virtual {
+        require(hasRole(MINTER_ROLE, _msgSender()), "DoTxNFT: must have minter role to mint");
+        
+        require(supply[houseId] != 0, "DoTxNFT: house does not exist");
+        require(nextHouseId[houseId] < supply[houseId], "DoTxNFT: house sold out");
+        uint256 tokenId = houseId * ID_TO_HOUSE + nextHouseId[houseId];
+        nextHouseId[houseId]++;
+
+        _mint(to, tokenId);
     }
 
-    function mint(address to, uint256 model) public override {
-        require(supply[model] != 0, "DoTxNFT: does not exist");
-        require(nextId[model] < supply[model], "DoTxNFT: sold out");
-        uint256 tokenId = model * ID_TO_MODEL + nextId[model];
-        nextId[model]++;
-        super.mint(to, tokenId);
+    function pause() public virtual {
+        require(hasRole(PAUSER_ROLE, _msgSender()), "DoTxNFT: must have pauser role to pause");
+        _pause();
+    }
+
+    function unpause() public virtual {
+        require(hasRole(PAUSER_ROLE, _msgSender()), "DoTxNFT: must have pauser role to unpause");
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override(ERC721, ERC721Pausable) {
+        super._beforeTokenTransfer(from, to, tokenId);
     }
 }
