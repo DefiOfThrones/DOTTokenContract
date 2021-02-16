c11c6591460549ac80651ecb5bc72906b117fb98
Maxime Reynders <maximereynders@gmail.com>
2021-02-16 17:12:48 +0100

    * Added NFT root

--- nftRoot/Context.sol
+++ nftRoot/Context.sol
@@ -1,5 +1,3 @@
-// SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
 /*
@@ -13,7 +11,7 @@ pragma solidity ^0.8.0;
  * This contract is only required for intermediate, library-like contracts.
  */
 abstract contract Context {
-    function _msgSender() internal view virtual returns (address payable) {
+    function _msgSender() internal view virtual returns (address) {
         return msg.sender;
     }
 
