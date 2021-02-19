483b6ead30d2e37b2980e40b63606d9d43d19461
Maxime Reynders <maximereynders@gmail.com>
2021-02-20 00:06:57 +0100

    * Fixed typo in address

--- nfts/childChain/ContextMixin.sol
+++ nfts/childChain/ContextMixin.sol
@@ -13,7 +13,7 @@ abstract contract ContextMixin {
                 // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
                 sender := and(
                     mload(add(array, index)),
-                    0xfffffffSfffffffffffffffffffffffffffffffff
+                    0xffffffffffffffffffffffffffffffffffffffff
                 )
             }
         } else {
